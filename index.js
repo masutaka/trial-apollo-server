ab7349679e1701ebd22ab3f71e580efcf01557f1
Takashi Masuda <masutaka.net@gmail.com>
2019-04-09 00:27:41 +0900

    Imprement typeDefs

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const {ApolloServer, gql} = require('apollo-server-express');
-const typeDefs = gql`
-  type Query {
-    content: String
-  }
-`;
-const resolvers = {
-  Query: {
-    content: () => 'こんにちは'
-  }
-};
+const {typeDefs} = require('./type_defs');
 
-const server = new ApolloServer({typeDefs, resolvers});
+// mocks: true で Mocking 機能を有効にする
+const server = new ApolloServer({typeDefs, mocks: true});
+
 const express = require('express');
 const app = express();
 server.applyMiddleware({app, path: '/graphql'});
